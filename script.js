2ac00493fde1eb7383b85f33f418c0283eef4a01
Himel <shahriarhasan221@gmail.com>
2025-08-29 10:08:15 +0600

    balance system added

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ document.getElementById("copy-btn").addEventListener("click", function () {
     document.getElementById("copy-count").innerText = copyCount + 1;
   });
 });
+
+// call button
+document.getElementById("call-btn").addEventListener("click", function () {
+  const callNumber = document.getElementById("call-content").innerText;
+  const copyNumber = document.getElementById("copy-content").innerText;
+  alert("Calling: " + callNumber + " - " + copyNumber);
+});
+// balance feature
+document.getElementById("call-btn").addEventListener("click", function () {
+  const balance = document.getElementById("call-currency");
+  let currency = parseInt(balance.innerText);
+  currency = Math.max(0, currency - 20); // Prevent negative currency
+  balance.innerText = currency;
+  if (currency === 0) {
+    alert("Insufficient balance");
+  }
+});
